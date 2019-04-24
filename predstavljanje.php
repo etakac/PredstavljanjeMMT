4cbe3697b5ccf62c21c2c0e66b99f36bd798223b
etakac <editakac@hotmail.com>
2019-04-24 23:30:33 +0200

    prvi commit

---
+++ predstavljanje.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Predstavljanje</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<style>
+body{
+	background-color: #3d3d3f;
+}
+.hr{
+	margin-top: -500px;
+}
+#h1{
+	margin-bottom: -5%;
+	margin-top: -3%;
+}
+.split {
+  
+  width: 50%;
+  position: absolute;
+  z-index: 1;
+  
+}
+
+.left {
+  left: 0;
+  margin-left: 8px;
+  
+}
+
+.right {
+  right: 0;
+  margin-right: 8px;
+}
+#p1{
+	font-size: 200%;
+	font-weight: bold;
+	margin-top: 20px;
+}
+
+#p2{
+	margin-left: 15px;
+	margin-top: 40px;
+	margin-right: 15px;
+	font-size: 115%;
+}
+#hr1{
+	margin-left: 8px;
+	margin-right: 8px;
+}
+
+#hr2{
+	width: 100;
+	margin-right: 35px;
+}
+
+#hr3{
+	width: 100;
+	margin-left: 35px;
+}
+#p3{
+	margin-left: 55px;
+	margin-top: 40px;
+	font-size: 115%;
+}
+#ho{
+	color:white;
+	font-weight: bold;
+}
+
+#ho:hover{
+	color:white;
+	font-weight: bold;
+}
+#de{
+	margin-left: 5px;
+}
+#dd{
+	background-color: #323335;
+	padding: -1%;
+	margin-top: 102%;
+	margin-left:1px;
+	margin-right: 1px;
+	height: 150px;
+}
+#p5{
+	margin-top: 100px;
+}
+#p6{
+	color: white;
+	font-weight: bold;
+	margin-left: 8px;
+	margin-top: 100px;
+}
+#edii{
+	margin-top: 25px;
+	margin-left: 15px;
+}
+#ediv{
+	margin-top: 20px;
+	margin-left: 15px;
+}
+#lukaa{
+	margin-top: 25px;
+	margin-left: 55px;
+}
+#lukavid{
+	margin-top: 20px;
+	margin-left: 55px;
+}
+</style>
+</head>
+
+<body>
+	<h2 id="h1" align="center"><img src="ferit.png" class="img-square" alt="ferit" style="width=300px; height: 350px;"></h2>
+	<hr id="hr1" style="border: 1px solid white; ">
+	<div class="split left">
+		<div class="centered">
+		<h2 id="edi" align="center"><a href="edi.png"><img src="edi.png" class="img-circle" alt="edi" style="width=150px;"/></a></h2>
+		<p id="p1" align="center" style="color: white;">Edi Takač</p>
+		</div>
+		<hr id="hr2" style="border: 1px solid white;">
+		<p id="p2" style="color: white;">Rođen sam 19. svibnja 1997. godine u <a id="ho" href="http://www.pozega.hr/">Požegi</a>. Trenutno živim u Osijeku zbog pohađanja fakulteta.</p>
+		<p id="p2" style="color: white;">Svoje školovanje sam započeo u<a id="ho" href="http://os-akanizlica-pozega.skole.hr/"> Osnovnoj školi Antuna Kanižlića</a> koju sam prošao sa odličnim uspjehom. Zbog tog uspjeha uspio sam upisati srednju<a id="ho" href="www.tehinckaskola.net/"> Tehničku školu u Požegi</a>, smjer Tehničar za računalstvo. Upisao sam Tehničku školu iz razloga što su me sve više zanimala računala i kako svijet funkcionira pomoću njih. Uspio sam završiti srednju školu sa vrlo dobrim uspjehom što mi je pomoglo pri upisivanju na fakultet. Nakon završetka srednje škole upisao sam <a id="ho" href="https://www.ferit.unios.hr/">Fakultet elektrotehnike, računarstva i informacijskih tehnologija</a> u Osijeku. Trenutno studiram na 3. godini na smjeru Informatika stručni studij. Na fakultetu sam stekao znanja iz programiranja u C,C++,Java i Python programskom jeziku. Također sam naučio raditi u HTML-u i CSS-u što mi je pomoglo pri izradi web stranica. Isto tako naučio sam šifrirati i dešifrirati poruke pomoću raznih načina.  </p>
+		<p id="p2" style="color: white;">Preko Studentskog servisa radio sam za firmu <a id="ho" href="https://www.valamar.com/">Valamar</a> koja je lanac hotela i kampova, gdje sam im pomagao u održavanju kampa. U slobodno vrijeme bavim se sportom i <a id="ho" href="https://www.esports.com/">e-sportovima</a>.</p>
+		<h2 id="de"><a href="deal.gif"><img src="deal.gif" class="img-sqaure" alt="deal" width="250" height="200" hspace="50"/></a><a href="buisness.jpg"><img src="buisness.jpg" class="img-sqaure" alt="buisness" width="250" height="200"/></a></h2>
+		<audio id="edii"  controls src="ed.mp3"></audio>
+		<p></p>
+		<video id="ediv" height="350" width="600" controls src="edivid.mp4"></video>
+	</div>
+	
+	<div class="split right">
+		<h2 id="luka" align="center"><a href="luka.jpg"><img src="luka.jpg" class="img-circle" alt="ferit" height="300" width="300"/></a></h2>
+		<p id="p1" align="center" style="color: white;">Luka Šporčić</p>
+		<hr id="hr3" style="border: 1px solid white;">
+		
+	<p id="p3" style="color: white;">Rođen 19.10.1997 u Požegi. Školovanje sam započeo u Područnoj školi Pleternice te <a id="ho" href="http://os-akanizlica-pozega.skole.hr/">OŠ Antuna Kanižlića</a> u Požegi. Za srednju školu odlučio sam se
+upisati u <a id="ho" href="www.tehinckaskola.net/">Tehničku školu</a>, smjer tehničar za računalstvo. Prolazio sam odličnim uspjehom. Sudjelovao sam u manjim projektima kroz koje smo proputovali
+Hrvatskom te u Erasmusu gdje smo boravili u Portugalu. Kroz sve projekte učili smo razne stvari te sam tako odlučio na <a id="ho" href="https://www.ferit.unios.hr/">FERIT</a> koji je imao povezanosti
+s naučenim stvarima. Koristim se osnovama programskih jezika kao što su C,C++,Java te Python te u Web programiranju koristim HTML,CSS,JS i PHP.
+U slobodno vrijeme bavim se Photoshopom. Fotografija koja mi je bila hobi sad je prerasla u profesionalno bavljenje. Uz to sve, bavim se nogometom.</p>
+	<h2 id="de"><a href="man.bmp"><img src="man.bmp" class="img-sqaure" alt="deal" width="250" height="200" hspace="100"/></a><a href="cam.png"><img src="cam.png" class="img-sqaure" alt="buisness" width="250" height="200"/></a></h2>
+	<audio id="lukaa" controls src="mmtaudio.mp3"></audio>
+	<p></p>
+	<video id="lukavid" height="350" width="600" controls src="lukavid.mp4"></video>
+	</div>
+	
+	<div id="dd">
+		<p id="p5" align="center" style="color: white;"> Web stranica je izrađena za vrijeme pohađanja <a id="ho" href="https://www.ferit.unios.hr/">Fakulteta elektrotehnike,
+računarstva i informacijskih tehnologija</a> u Osijeku u sklopu kolegija<a id="ho" href="https://loomen.carnet.hr/course/view.php?id=3626"> Multimedijska tehnika</a></p>
+		<p id="p6">Kolege: <a id="ho" href="https://multimedijskatehnika.000webhostapp.com/">Dario i Robert</a> && <a id="ho" href="http://mmt.tim.com.hr/#tkalcec">Dominik i Ivan</a></p>
+	</div>
+	
+
+
+</body>
+</html>
